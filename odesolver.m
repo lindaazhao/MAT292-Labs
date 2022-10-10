3c816fa38d3b2d3e1feb89deb3b52b62d94ba9d1
Linda Zhao <linda.zhao23@gmail.com>
2022-10-10 00:04:02 -0400

    Update lab 3; fix odesolver fcn

--- odesolver.m
+++ odesolver.m
@@ -3,10 +3,6 @@ function [odesoln] = odesolver(f1, t0, tN, y0, h)
 % [t0, tN] = internal on which to solve the ODE
 % y0 = initial condition of the ODE
 % h = step size
-    if mod(tN-t0, h) ~= 0                           % Range does not fit an integer multiple of the step size
-        tN = t0 + (floor((tN-t0)/h) + 1) * h;       % Increase tN such that it now fits an integer multiple of h
-    end
-    
     soln_t = t0:h:tN;
     
     odesoln = NaN(1, length(soln_t));
